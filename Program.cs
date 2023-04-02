2f32513a35c6150e219134dce491a8d4199ba863
Mrunmayee29 <87325056+Mruna29@users.noreply.github.com>
2023-04-02 11:28:41 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeCasting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your First number:");
+            int value1=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your second number");
+            int value2=Convert.ToInt32(Console.ReadLine());
+            //Console.ReadLine();
+
+            int addition= value1 + value2;
+            Console.WriteLine("The Addition is = " + addition);
+            Console.ReadLine();
+
+        }
+    }
+}
