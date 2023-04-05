09ad4cc4c470624af09f715ab86531576454408a
Mrunmayee29 <87325056+Mruna29@users.noreply.github.com>
2023-04-05 21:00:11 +0530

    Create ClassObjectCreation.cs
    simple program -object creation to access the class members

---
+++ ClassObjectCreation.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassObjectCreation
+{
+    class student
+    {
+        //creating class members that is feild and methods
+        public int id = 101;
+        public string name = "Mrunmayee";
+        public int age = 27;
+
+        public void mymethod()
+        {
+            Console.WriteLine("Name of student is " + name  +  "\nand id is " + id);
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //creating of object
+            student s1=new student();
+            s1.mymethod();
+            Console.ReadLine();
+        }
+    }
+}
